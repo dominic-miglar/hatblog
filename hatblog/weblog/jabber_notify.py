1ad98be24bfd53d8b23458e6392a9cd8bcb527d6
Dominic Miglar <dominic.miglar@w1r3.net>
2013-06-12 17:51:31 +0200

    fixed jabber_notify

--- hatblog/weblog/jabber_notify.py
+++ hatblog/weblog/jabber_notify.py
@@ -6,9 +6,11 @@ from hatblog.settings import JABBER_ID, JABBER_PASSWORD, JABBER_MSG_RECIPIENT
 
 def jabber_notify(message):
 	jid = xmpp.protocol.JID(JABBER_ID)
-	#cl = xmpp.Client(jid.getDomain(), debug=[])
-	cl = xmpp.Client(jid.getDomain())
-	cl.connect()
-	cl.auth(jid.getNode(), JABBER_PASSWORD)
+	cl = xmpp.Client(jid.getDomain(), debug=[])
+	if cl.connect() == '':
+		return
+	if cl.auth(jid.getNode(), JABBER_PASSWORD) == None:
+		return
 	cl.send(xmpp.protocol.Message('firehat@w1r3.net', message))
 	cl.disconnect()
+
