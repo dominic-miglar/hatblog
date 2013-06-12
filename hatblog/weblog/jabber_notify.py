7fcc0c7524776bc4a82870700ec320dd65bf2a60
Dominic Miglar <dominic.miglar@w1r3.net>
2013-06-12 16:31:05 +0200

    added jabber_notify

--- hatblog/weblog/jabber_notify.py
+++ hatblog/weblog/jabber_notify.py
@@ -2,7 +2,12 @@
 #-*- coding:utf-8 -*-
 
 import xmpp
-from hatblog.settings import JABBER_ID, JABBER_PASSWORD, JABBER_MSG_RECIPIENT
+#from hatblog.settings import JABBER_ID, JABBER_PASSWORD, JABBER_MSG_RECIPIENT
+
+JABBER_ID = 'hatblog@w1r3.net'
+JABBER_PASSWORD = 'hat.blog@work.31337'
+JABBER_MSG_RECIPIENT = 'firehat@w1r3.net'
+
 
 def jabber_notify(message):
 	jid = xmpp.protocol.JID(JABBER_ID)
@@ -11,4 +16,3 @@ def jabber_notify(message):
 	cl.auth(jid.getNode(), JABBER_PASSWORD, 'hatblog')
 	cl.send(xmpp.protocol.Message('firehat@w1r3.net', message))
 	cl.disconnect()
-	
